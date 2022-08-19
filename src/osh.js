48c0079d54451f2e90c721ac370f53c7cb97f33a
Wafie <wafiuddin98@gmail.com>
2022-08-19 15:29:49 +0800

    websocket-try2

--- src/osh.js
+++ src/osh.js
@@ -7,21 +7,26 @@ import {
   import { Helmet } from "react-helmet"
 
 class Osh extends Component {
+    constructor(props) {
+        super(props);
+     
+        this.canvas = React.createRef();
+     }
 
+    
 
     render(){
         let uri = "ws://" + window.location.hostname + ":8585";
         const socket = new WebSocket(uri);
-         let msg = document.getElementById("msg");
          socket.addEventListener('open', (e) => {
              document.getElementById("status").innerHTML = "Opened";
          });
          socket.addEventListener('message', (e) => {
-             let ctx = msg.getContext("2d");
+             let ctx = this.canvas.current.getContext("2d");
              let image = new Image();
              image.src = URL.createObjectURL(e.data);
              image.addEventListener("load", (e) => {
-                 ctx.drawImage(image, 0, 0, msg.width, msg.height);
+                 ctx.drawImage(image, 0, 0, this.canvas.current.width, this.canvas.current.height);
              });
          });
         return (
@@ -33,7 +38,7 @@ class Osh extends Component {
             <ReflexElement className="left-pane">
                 <div className="pane-content" style={{color:"white"}}>Livestream
                     <div id="status" style={{color:"white"}}>Connection failed.</div>
-                    <canvas id="msg" width="960" height="720"/>
+                    <canvas ref={this.canvas} width="960" height="720"/>
                 </div>
             </ReflexElement>
             <ReflexSplitter/>
