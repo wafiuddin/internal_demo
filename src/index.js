5345988f052ffe9f18cc92176a7b79b46b9edc3c
Wafie <wafiuddin98@gmail.com>
2022-08-19 10:43:51 +0800

    initial

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Osh from './osh';
+import Osh from './Osh';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
