531e38cfa5c3d78e94650d107d20d9c3eecf4732
Wafie <wafiuddin98@gmail.com>
2022-08-19 14:33:33 +0800

    websocket 2nd try

--- src/osh.js
+++ src/osh.js
@@ -8,24 +8,22 @@ import {
 
 class Osh extends Component {
 
-    componentDidMount = () => {
-        let uri = "ws://" + window.location.hostname + ":8585";
-       const socket = new WebSocket(uri);
-        let msg = document.getElementById("msg");
-        socket.addEventListener('open', (e) => {
-            document.getElementById("status").innerHTML = "Opened";
-        });
-        socket.addEventListener('message', (e) => {
-            let ctx = msg.getContext("2d");
-            let image = new Image();
-            image.src = URL.createObjectURL(e.data);
-            image.addEventListener("load", (e) => {
-                ctx.drawImage(image, 0, 0, msg.width, msg.height);
-            });
-        });
-    }
 
     render(){
+        let uri = "ws://" + window.location.hostname + ":8585";
+        const socket = new WebSocket(uri);
+         let msg = document.getElementById("msg");
+         socket.addEventListener('open', (e) => {
+             document.getElementById("status").innerHTML = "Opened";
+         });
+         socket.addEventListener('message', (e) => {
+             let ctx = msg.getContext("2d");
+             let image = new Image();
+             image.src = URL.createObjectURL(e.data);
+             image.addEventListener("load", (e) => {
+                 ctx.drawImage(image, 0, 0, msg.width, msg.height);
+             });
+         });
         return (
             <div>
             <Helmet>
