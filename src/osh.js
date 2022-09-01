9336aba4ebd4f21d1583e20602764f11924a5844
Wafie <wafiuddin98@gmail.com>
2022-09-01 17:01:59 +0800

    ui

--- src/osh.js
+++ src/osh.js
@@ -84,7 +84,7 @@ const darkTheme = createTheme({
       
         render() {
           return (
-            <img src={this.state.path} width="440" height="600" style={{objectFit:"contain"}}/>
+            <img src={this.state.path} width="400" height="600" style={{objectFit:"contain"}}/>
           );
         }
     
